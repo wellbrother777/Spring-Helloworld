c5324d23077a8af008ac7dfbd59cbb280896d7de
serg <svmodin@mail.ru>
2021-01-07 12:59:18 -0800

    Практическая задача 2.1.2

---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+import java.util.Objects;
+
+public class Cat {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
